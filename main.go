1e36b722a8332a7ff38516c04580229a14256897
licat <1697470538@qq.com>
2020-06-06 15:33:36 +0800

    add htmlfile

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 var port = flag.String("port", ":8080", "http service port")
+var ssl = flag.String("ssl", "ws", "ssl = wss")
 var wsUpgrader = websocket.FastHTTPUpgrader{
 	// 解决跨域问题
 	CheckOrigin: func(ctx *fasthttp.RequestCtx) bool {
@@ -119,70 +120,16 @@ func indexView(ctx *fasthttp.RequestCtx) {
 		i++
 		jsstr = jsstr + `<tr><td>` + k + `<\/td><td id=web` + strconv.Itoa(i) + ` >` + strconv.Itoa(v) + `<\/td><\/tr>`
 		wsstr = `web` + strconv.Itoa(i)
-		htmstr = htmstr + wsstr + `=new WebSocket("wss://` + *addr + `/weblistServer");` + wsstr + `.onopen = function (evt){` + wsstr + `.send("` + k + `");};` + wsstr + `.onmessage=function(e){document.getElementById("` + wsstr + `").innerHTML=e.data};
-`
+		htmstr = htmstr + wsstr + `=new WebSocket("`+*ssl+`://` + *addr + `/weblistServer");
+` + wsstr + `.onopen = function (){` + wsstr + `.send("` + k + `");};
+` + wsstr + `.onmessage=function(e){document.getElementById("` + wsstr + `").innerHTML=e.data};`
 	}
-	indexTemplate := template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="https://licat.work/favicon.ico">
-    <title>享购推广-网站在线人数监控面板</title>
-<script>
-window.addEventListener("load", function (evt) {
-            const weblist = document.getElementById("weblist");
-let wdt = "` + jsstr + `";
-let weblistdata = wdt == ""?"<tr><td colspan='2'>ㄟ( ▔, ▔ )ㄏ<br>空空如也<\/td><\/tr>":wdt;
-weblist.insertAdjacentHTML("beforeend",weblistdata);
-` + htmstr + `
-        });
-</script>
-<style>
-.title{text-align:center;color:#00e6ff;letter-spacing:0;text-shadow:0px 1px 0px #999,0px 2px 0px #888,0px 3px 0px #777,0px 4px 0px #666,0px 5px 0px #555,0px 6px 0px #444,0px 7px 0px #333,0px 8px 7px #001135;}
-table{font-family:sans-serif;border-collapse:collapse;margin:0 auto;text-align:center;}
-table td,table th{border:1px solid #cad9ea;color:#666;height:30px;}
-table thead th{background-color:#CCE8EB;}
-table tr:nth-child(odd){background:#fff;}
-table tr:nth-child(even){background:#F5FAFA;}
-.boxsha{box-shadow:0px 1px 0px #999,0px 2px 0px #888,0px 3px 0px #777,0px 4px 0px #666,0px 5px 0px #555,0px 6px 0px #444,0px 7px 0px #333,0px 8px 7px #001135;width:100%;}
-body{
-    margin: 0 auto;
-    width: 100%;
-    max-width: 800px;
-    box-sizing: border-box;
-    padding: 10px;
-}
-td:hover {
-    -webkit-transform: translateY(-3px);
-    -ms-transform: translateY(-3px);
-    transform: translateY(-3px);
-    text-shadow: 0px 1px 0px #999, 0px 2px 0px #888, 0px 3px 3px #777;
-    color: #00e6ff;
-}
-th:hover{
-    text-shadow: 0px 1px 0px #999, 0px 2px 0px #888, 0px 3px 3px #777;
-    color: #00e6ff;
-}
-th,td{
-    cursor: pointer;
-}
-</style>
-</head>
-<body>
-<h1 class=title>享购推广</h1>
-<table class=boxsha>
-<tbody id=weblist>
-<thead>
-<th width=70%>Web Url</th>
-<th width=30%>OnLine Num</th>
-</thead>
-</tbody>
-</table>
-</body>
-</html>
-`))
+	JS1 := template.JS(jsstr)
+	JS2 := template.JS(htmstr)
+	indexTemplate := template.Must(template.ParseFiles("./static/index.html"))
 	ctx.SetContentType("text/html")
-	indexTemplate.Execute(ctx, nil)
+	indexTemplate.Execute(ctx, struct{JScode1 template.JS;JScode2 template.JS}{JS1,JS2})
+	jsstr = ""
+	htmstr = ""
+	backdata = make(map[string]int)
 }
